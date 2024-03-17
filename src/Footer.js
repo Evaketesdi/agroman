4d75a2476584b79c8a1177661988b70199d9ce99
Evaketesdi <evahollo85@yahoo.com>
2024-03-17 19:27:20 +0200

    testing

--- src/Footer.js
+++ src/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="my-5 text-color">
+    <footer id="footer" className="container my-5 text-color">
       <div className="container">
         <strong>AGROMAN</strong>
         <p>You can find us in Romania</p>
         <strong>Get connected with us</strong>
-        <p className="lead">&copy 2023 Copyright: Agroman</p>
+        <p className="lead">© 2023 Copyright: Agroman</p>
         <p className="lead">All rights reserved</p>
       </div>
     </footer>
